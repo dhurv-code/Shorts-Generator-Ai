340b4362efb153704e6bf87b9d0be579db3ddd75
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-06-10 12:21:30 +0530

    update cors

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 import os
-
 from app.api.video_routes import router
+
 
 os.makedirs("outputs", exist_ok=True)
 
+
 app = FastAPI(
     title="AI Shorts Generator"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://shorts-generator-ai.vercel.app/"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
